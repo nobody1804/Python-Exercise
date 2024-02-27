9d8facda8db62a382234dc31bf0226e186f53256
nobody1804 <aryamanvijay18@gmail.com>
2024-02-27 17:48:22 +0530

    Some exercise of python

---
+++ HCF of a number.py
@@ -0,0 +1,8 @@
+num1 = 70
+num2 = 15
+hcf = 1
+
+for i in range(1, min(num1, num2)):
+    if num1 % i == 0 and num2 % i == 0:
+        hcf = i
+print(hcf)
---
+++ LIFE IN WEEKS.py
@@ -0,0 +1,5 @@
+age = input()
+years = 90 - int(age)
+weeks = years * 52
+
+print(f"You have {weeks} weeks left.")
