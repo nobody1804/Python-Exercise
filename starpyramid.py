a071cc9702956f8b2191d460d74c1eda151e913f
nobody1804 <aryamanvijay18@gmail.com>
2024-02-28 22:23:49 +0530

    Start pyramid practice

--- starpyramid.py
+++ starpyramid.py
@@ -1,3 +1,11 @@
-for i in range(1, 10):
-    str = "* "
-    print(str * i)
+# # for i in range(1, 10):
+# #     str = "*"              ##star pyramid in simple
+# #     print(str * i)
+
+def pyramid(rows):                  ##starpyramid but more accurate
+    for i in range(1, rows+1):
+        print(" " * (rows - i), end="")
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print()
+pyramid(10)
